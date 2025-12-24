be7b5c354bdd2bd2351ba710fe45bd2c5bb21552
fmbp1981-hash <fmbp1981@gmail.com>
2025-12-23 21:32:13 -0300

    fix: set outputFileTracingRoot for subfolder workspace

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: __dirname,
 };
 
 export default nextConfig;
